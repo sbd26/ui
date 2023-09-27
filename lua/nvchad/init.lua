511566fcdf2796af536c69618269bbd3b9951aaa
sikder <shbadhon10@gmail.com>
2023-09-28 00:34:02 +0600

    Removed Statusline

--- lua/nvchad/init.lua
+++ lua/nvchad/init.lua
@@ -1,9 +1,6 @@
 local new_cmd = vim.api.nvim_create_user_command
 local config = require("core.utils").load_config().ui
 
-vim.opt.statusline = "%!v:lua.require('nvchad.statusline." .. config.statusline.theme .. "').run()"
-
-
 -- command to toggle cheatsheet
 new_cmd("NvCheatsheet", function()
   if vim.g.nvcheatsheet_displayed then
@@ -13,11 +10,3 @@ new_cmd("NvCheatsheet", function()
   end
 end, {})
 
-
--- redraw statusline on LspProgressUpdate event & fixes #145
-vim.api.nvim_create_autocmd("User", {
-  pattern = "LspProgressUpdate",
-  callback = function()
-    vim.cmd "redrawstatus"
-  end,
-})
